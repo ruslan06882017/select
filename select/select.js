4a12e7fe2a28be92ef400a7ba5c1911def7f1124
Ruslan <r.gusseinov@mail.ru>
2020-09-12 01:25:23 +0600

    Completed select plugin

--- select/select.js
+++ select/select.js
@@ -1,19 +1,26 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = ''
+  console.log(selectedId)
+  if (placeholder) text = placeholder; else text = 'Нифига нет'
+  const items = data.map(item => {
+    let cls = ''
+    if (selectedId === item.id) {
+      text = item.value
+      cls = 'selected'
+    }
+    return `
+      <li class="select__item ${cls}" data-type="item" data-id=${item.id}>${item.value}</li>
+    `
+  })
+
   return `
     <div class="select__input" data-type="input">
-      <span>Text</span>
+      <span data-type="value">${text}</span>
       <i class="da" data-type="arrow"></i>
      </div>
   <div class="select__dropdown">
     <ul class="select__list">
-      <li class="select__item">123</li>
-      <li class="select__item">556</li>
-      <li class="select__item">364</li>
-      <li class="select__item">364</li>
-      <li class="select__item">364</li>
-      <li class="select__item">364</li>
-      <li class="select__item">364</li>
-      <li class="select__item">364</li>
+      ${items.join('')}
     </ul>
   </div>
   `
@@ -22,30 +29,57 @@ const getTemplate = () => {
 export class Select{
   constructor(selector, options){
     this.$el = document.querySelector(selector)
+    this.options = options
+    this.selectedId = options.selectedId
     this.render()
     this.setup()
   }
 
   render(){
-    this.$el.classList.add('select')
-    this.$el.innerHTML = getTemplate()
+    const {placeholder, data, selectedId} = this.options 
+    this.$el.classList.add('select')  
+    this.$el.innerHTML = getTemplate(data, placeholder, selectedId)
   }
 
   setup(){
     this.clickHandler = this.clickHandler.bind(this)
     this.$el.addEventListener('click', this.clickHandler)
     this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+    this.$value = this.$el.querySelector('[data-type="value"]')
   }
 
   clickHandler(e){
     const {type} = event.target.dataset
     if (type === 'input'){
       this.toggle()
+    } else if (type === 'item'){
+      const id = event.target.dataset.id
+      // console.log('id', id)
+      this.select(id)
     }
   }
 
   get isOpen(){
     return this.$el.classList.contains('open')
+  }
+
+  get current(){
+    return this.options.data.find(item => item.id === this.selectedId)
+  }
+
+  select(id){
+    this.selectedId = id
+    this.$value.textContent = this.current.value
+   
+    const els = this.$el.querySelectorAll(`.select__item`)
+    els.forEach(element => {
+        element.classList.remove('selected')
+    });
+    
+    this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+    this.options.onSelect ? this.options.onSelect(this.current) : null
+    
+    this.close()
   }
 
   toggle(){
@@ -66,6 +100,7 @@ export class Select{
 
   destroy(){
     this.$el.removeEventListener('click', this.clickHandler)
+    this.$el.innerHTML = ''
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import {Select} from '../select/select'
 import '../src/main.scss'
 const select = new Select('#select', {
-
+  selectedId: '2',
+  placeholder: 'Вывберите значение',
+  data: [
+    {id: '1', value: 'React'},
+    {id: '2', value: 'Vue'},
+    {id: '3', value: 'PHP'},
+    {id: '4', value: 'CSS'},
+    {id: '5', value: 'React'}
+  ],
+  onSelect(item){
+    console.log(`Selected Item`, item)
+  }
 })
 
 window.s = select
